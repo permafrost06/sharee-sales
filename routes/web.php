ec1a5ed5d80b36bf9b40625773ae223690b912e7
Saad <sakib.saad.khan@gmail.com>
2023-02-11 07:50:11 +0600

    [*] Updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\admin\CustomerController;
+use App\Http\Controllers\admin\IndexController;
+use App\Http\Controllers\admin\PurchaseController;
+use App\Http\Controllers\admin\SalesController;
+use App\Http\Controllers\admin\VendorController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +20,56 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+Route::get('/test', 'api\v1\TestController@test');
+Route::prefix('/admin')->middleware(['auth', 'admin'])->group(function () {
+    Route::get('/', [IndexController::class, 'index'])->name('admin.index');
+    Route::get('/search', ['uses' => 'admin\SearchController@searchQuestion', 'as' => 'question.search']);
+
+    Route::controller(CustomerController::class)->middleware('admin')->group(function () {
+        Route::get('/customers', ['uses' => 'index', 'as' => 'customers.index']);
+        Route::get('/create/customers', ['uses' => 'create', 'as' => 'customers.create']);
+        Route::get('/edit/customers', ['uses' => 'edit', 'as' => 'customers.edit']);
+        Route::post('/store/customers', ['uses' => 'store', 'as' => 'customers.store']);
+        Route::post('/update/customers', ['uses' => 'update', 'as' => 'customers.update']);
+        Route::get('/delete/customer', ['uses' => 'delete', 'as' => 'customers.delete']);
+    });
+
+    Route::controller(SalesController::class)->middleware('admin')->group(function () {
+        Route::get('/sales', ['uses' => 'index', 'as' => 'sales.index']);
+        Route::get('/download', ['uses' => 'generatePDF', 'as' => 'sales.generatePDF']);
+        Route::get('/create/sales', ['uses' => 'create', 'as' => 'sales.create']);
+        Route::get('/edit/sales', ['uses' => 'edit', 'as' => 'sales.edit']);
+        Route::post('/store/sales', ['uses' => 'store', 'as' => 'sales.store']);
+        Route::post('/update', ['uses' => 'update', 'as' => 'sales.update']);
+        Route::get('/delete/sales', ['uses' => 'delete', 'as' => 'sales.delete']);
+        Route::get('/delete/multiple', ['uses' => 'deleteMultiple', 'as' => 'sales.delete.multiple']);
+    });
+
+    Route::controller(VendorController::class)->middleware('admin')->group(function () {
+        Route::get('/vendors', ['uses' => 'index', 'as' => 'vendor.index']);
+        Route::get('/create/vendor', ['uses' => 'create', 'as' => 'vendor.create']);
+        Route::get('/edit/vendor', ['uses' => 'edit', 'as' => 'vendor.edit']);
+        Route::post('/store/vendor', ['uses' => 'store', 'as' => 'vendor.store']);
+        Route::post('/update/vendor', ['uses' => 'update', 'as' => 'vendor.update']);
+        Route::get('/delete/vendor', ['uses' => 'delete', 'as' => 'vendor.delete']);
+        Route::get('/delete/multiple/vendor', ['uses' => 'deleteMultiple', 'as' => 'vendor.delete.multiple']);
+    });
+
+    Route::controller(PurchaseController::class)->middleware('admin')->group(function () {
+        Route::get('/purchase', ['uses' => 'index', 'as' => 'purchase.index']);
+        Route::get('/download-purchase-report', ['uses' => 'generatePDF', 'as' => 'purchase.generatePDF']);
+        Route::get('/create/purchase', ['uses' => 'create', 'as' => 'purchase.create']);
+        Route::get('/edit/purchase', ['uses' => 'edit', 'as' => 'purchase.edit']);
+        Route::post('/store/purchase', ['uses' => 'store', 'as' => 'purchase.store']);
+        Route::post('/update/purchase', ['uses' => 'update', 'as' => 'purchase.update']);
+        Route::get('/delete/purchase', ['uses' => 'delete', 'as' => 'purchase.delete']);
+        Route::get('/delete/multiple/purchase', ['uses' => 'deleteMultiple', 'as' => 'purchase.delete.multiple']);
+    });
+});
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+include __DIR__ . '/auth.php';
