5d9ab4735da3ae0ca4d5f340e5a483d16077fc49
Saad <sakib.saad.khan@gmail.com>
2023-02-14 11:13:34 +0600

    insert 2nd admin with password:password

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         DB::insert(
             "INSERT INTO `users` (`id`, `name`, `username`, `phone`, `type`, `gender`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-            (1, 'admin', NULL, NULL, 'admin', NULL, 'admin@admin.com', NULL, '$2y$10\$pqSsKcTaktmaiubX/.ZhP.6fuz8IxHqyo8jBiiB3YEMwCUFCn1CrW', 'woKxxNKYi4QEDvmi0tjqJbx8CqibJonpUHE10E4rgYQHAPApi7jfLqA5kSHP', '2019-09-09 23:10:28', '2019-09-09 23:10:28')"
+            (1, 'admin', NULL, NULL, 'admin', NULL, 'admin@admin.com', NULL, '\$2y$10\$pqSsKcTaktmaiubX/.ZhP.6fuz8IxHqyo8jBiiB3YEMwCUFCn1CrW', 'woKxxNKYi4QEDvmi0tjqJbx8CqibJonpUHE10E4rgYQHAPApi7jfLqA5kSHP', '2019-09-09 23:10:28', '2019-09-09 23:10:28'),
+            (2, 'Saad', NULL, NULL, 'admin', NULL, 'test@example.com', NULL, '\$2y$10\$OErG.mAlpq7cOWNZnEV5GOkpBBu0MpxHA9n1Rug.F/NOWIaqMPqN2', 'isthisnecessary', '2019-09-09 23:10:28', '2019-09-09 23:10:28')"
         );
 
         DB::insert(
