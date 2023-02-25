d9cd1e589047543b8ac87c7650c2b4e4f97985d0
Zahin Azmayeen <zahin06@gmail.com>
2023-02-25 19:45:54 +0600

    rename column Quantity -> In Stock and move

--- resources/views/admin/stocks/status.blade.php
+++ resources/views/admin/stocks/status.blade.php
@@ -34,9 +34,9 @@
                             <tr>
                                 <th>Sln</th>
                                 <th>Item Code</th>
-                                <th>Quantity</th>
                                 <th>Total In</th>
                                 <th>Total Out</th>
+                                <th>In Stock</th>
                                 <th>Attachments</th>
                                 <th>Remarks</th>
                             </tr>
@@ -48,9 +48,9 @@
                                 <td>
                                     <a href="{{route('stocks.logs', ['item' => $stock->item_code])}}">{{ $stock->item_code }}</a>
                                 </td>
-                                <td> {{ $stock->total_quantity }}</td>
                                 <td> {{ $stock->total_in }}</td>
                                 <td> {{ $stock->total_out }}</td>
+                                <td> {{ $stock->total_quantity }}</td>
                                 <td>
                                     @if($stock->item?->attachment)
                                     <a href="#" data-toggle="modal" data-target="#modal-attachment" data-item="{{$stock->item_code}}" data-src="{{asset($stock->item?->attachment)}}" class="show-attachment-modal">View</a>
