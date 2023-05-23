1720f5020bb6dcfb81d73808b5f6dfa1da3f1150
Saad <sakib.saad.khan@gmail.com>
2023-05-23 12:37:22 +0600

    commented default admin pass

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,12 +18,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        /**
+         * Admin password: 123456
+         */
         DB::insert(
             "INSERT INTO `users` (`id`, `name`, `username`, `phone`, `type`, `gender`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
             (1, 'admin', NULL, NULL, 'admin', NULL, 'admin@admin.com', NULL, '\$2y$10\$pqSsKcTaktmaiubX/.ZhP.6fuz8IxHqyo8jBiiB3YEMwCUFCn1CrW', 'woKxxNKYi4QEDvmi0tjqJbx8CqibJonpUHE10E4rgYQHAPApi7jfLqA5kSHP', '2019-09-09 23:10:28', '2019-09-09 23:10:28'),
