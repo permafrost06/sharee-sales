2ab8af88c0099e27fd51c5073866f1f27f190f1b
Saad <sakib.saad.khan@gmail.com>
2023-05-26 07:50:04 +0600

    added SidebarItem

---
+++ resources/views/components/sidebar-item.blade.php
@@ -0,0 +1,15 @@
+<li class="my-2">
+    <a href="#" @class([
+        'flex items-center p-2',
+        'rounded-md bg-sidebar-active text-active' => $active,
+    ])>
+        @isset($svg)
+            <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="{{$svg->attributes->get('viewBox', '0 0 24 24')}}">
+                {{ $svg }}
+            </svg>
+        @endisset
+        <div class="flex-grow font-medium">
+            {{ $slot }}
+        </div>
+    </a>
+</li>
