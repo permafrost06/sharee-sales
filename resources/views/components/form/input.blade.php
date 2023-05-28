8cdfc58412c6756483570e265c17deeeaaf87762
Saad <sakib.saad.khan@gmail.com>
2023-05-28 13:15:22 +0600

    added form components

---
+++ resources/views/components/form/input.blade.php
@@ -0,0 +1,22 @@
+<div @class($class) id="{!!$groupId!!}">
+    @if ($label)
+    <label for="{!!$id!!}" class="mb-2 ml-1 font-bold text-xs text-slate-700">{!!$label!!}</label>
+    @endif
+    <input
+        id="{!!$id!!}"
+        class="focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
+        placeholder="{{$placeholder??$label}}" {{$attributes}}/>
+    @if ($hint)
+    <label
+        for="{!!$id!!}"
+        @class([
+            "text-xs font-normal leading-normal mt-0",
+            'text-red-500' => $hintType == 'error',
+            'text-yellow-300' => $hintType == 'warning',
+            'text-emerald-600' => $hintType == 'success',
+            'text-gray-600' => $hintType == 'none'
+        ])>
+        {{$hint}}
+    </label>
+    @endif
+</div>
