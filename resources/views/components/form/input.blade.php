bf1fce39d12be705b668e940c1d90192e36b362d
Saad <sakib.saad.khan@gmail.com>
2023-06-10 12:40:55 +0600

    added auto error detection

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -6,6 +6,16 @@
         id="{!!$id!!}"
         class="focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
         placeholder="{{$placeholder??$label}}" {{$attributes}}/>
+    @php
+        $name = $attributes->get('name', '');
+        if (
+            !$hint &&
+            $hintType === 'error' &&
+            $errors->has($name)
+        ) {
+            $hint = $errors->first($name);
+        }
+    @endphp
     @if ($hint)
     <label
         for="{!!$id!!}"
